6171849db04161b7aa80dd3364f4910b7b3bba52
Suada Haji <suada.haji@andela.com>
2018-08-28 13:58:11 +0300

    Create the RegisterVisitor Activity

---
+++ app/src/main/java/com/haji/suada/visitorstracker/RegisterVisitorActivity.java
@@ -0,0 +1,13 @@
+package com.haji.suada.visitorstracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterVisitorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_visitor);
+    }
+}
