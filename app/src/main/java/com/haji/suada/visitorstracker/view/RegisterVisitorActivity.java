c52be93fea5e626a9a1d2a1627dac64ff277d2d2
Suada Haji <suada.haji@andela.com>
2018-08-28 18:08:54 +0300

    Move activities to ui folder

--- app/src/main/java/com/haji/suada/visitorstracker/view/RegisterVisitorActivity.java
+++ app/src/main/java/com/haji/suada/visitorstracker/view/RegisterVisitorActivity.java
@@ -1,7 +1,9 @@
-package com.haji.suada.visitorstracker;
+package com.haji.suada.visitorstracker.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.haji.suada.visitorstracker.R;
 
 public class RegisterVisitorActivity extends AppCompatActivity {
 
